2b0b9a032976ee647bdfdfc0ba49506624edf67e
pan4kk21 <92604427+pan4kk21@users.noreply.github.com>
2024-02-13 16:45:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
 var buttons = document.querySelectorAll('button');
 
+function audioPlay (key) {
+    switch (key) {
+        case 'w':
+            new Audio('./sounds/tom-1.mp3').play();
+            break;
+        
+        case 'a':
+            new Audio('./sounds/tom-2.mp3').play();
+            break;
+
+        case 's':
+            new Audio('./sounds/tom-3.mp3').play();
+            break;
+
+        case 'd':
+            new Audio('./sounds/tom-4.mp3').play();
+            break;
+
+        case 'j':
+            new Audio('./sounds/snare.mp3').play();
+            break;
+
+        case 'k':
+            new Audio('./sounds/crash.mp3').play();
+            break;
+
+        case 'l':
+            new Audio('./sounds/kick-bass.mp3').play();
+            break;
+
+        default:
+            console.log(key);
+    }
+}
+
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function() {
-        switch (this.innerHTML) {
-            case 'w':
-                new Audio('./sounds/tom-1.mp3').play();
-                break;
-            
-            case 'a':
-                new Audio('./sounds/tom-2.mp3').play();
-                break;
-
-            case 's':
-                new Audio('./sounds/tom-3.mp3').play();
-                break;
-
-            case 'd':
-                new Audio('./sounds/tom-4.mp3').play();
-                break;
-
-            case 'j':
-                new Audio('./sounds/snare.mp3').play();
-                break;
-
-            case 'k':
-                new Audio('./sounds/crash.mp3').play();
-                break;
-
-            case 'l':
-                new Audio('./sounds/kick-bass.mp3').play();
-                break;
-
-            default:
-                console.log(this.innerHTML);
-        }
+        audioPlay(this.innerHTML);
+        buttonAnimation(this.innerHTML);
     });
+}
+
+document.addEventListener('keydown', function(event) {
+    audioPlay(event.key);
+    buttonAnimation(this.innerHTML);
+})
+
+function buttonAnimation(key) {
+    document.querySelector('.' + key).classList.add('pressed');
+    setTimeout(function() {
+        document.querySelector('.' + key).classList.remove('pressed');
+    }, 100);
 }
